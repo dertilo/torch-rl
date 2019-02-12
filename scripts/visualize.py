b22d4fd474b7b941834925cedc63167b76fff499
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-02-12 14:46:45 +0100

    visualize-script

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -11,53 +11,51 @@ except ImportError:
 
 import utils
 
-# Parse arguments
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--env", required=True,
-                    help="name of the environment to be run (REQUIRED)")
-parser.add_argument("--model", required=True,
-                    help="name of the trained model (REQUIRED)")
-parser.add_argument("--seed", type=int, default=0,
-                    help="random seed (default: 0)")
-parser.add_argument("--shift", type=int, default=0,
-                    help="number of times the environment is reset at the beginning (default: 0)")
-parser.add_argument("--argmax", action="store_true", default=False,
-                    help="select the action with highest probability")
-parser.add_argument("--pause", type=float, default=0.1,
-                    help="pause duration between two consequent actions of the agent")
-args = parser.parse_args()
 
-# Set seed for all randomness sources
+def visualize_it(env_name,model_file,pause_dur=0.1,seed=0,shift=0,argmax=False):
+    # Set seed for all randomness sources
+    utils.seed(seed)
+    # Generate environment
+    env = gym.make(env_name)
+    env.seed(seed)
+    for _ in range(shift):
+        env.reset()
+    # Define agent
+    # model_dir = utils.get_model_dir(model_file)
+    agent = utils.Agent(env_name, env.observation_space, model_file, argmax)
+    # Run the agent
+    done = True
+    while True:
+        if done:
+            obs = env.reset()
 
-utils.seed(args.seed)
+        time.sleep(pause_dur)
+        renderer = env.render()
 
-# Generate environment
+        action = agent.get_action(obs)
+        obs, reward, done, _ = env.step(action)
+        agent.analyze_feedback(reward, done)
 
-env = gym.make(args.env)
-env.seed(args.seed)
-for _ in range(args.shift):
-    env.reset()
+        if renderer.window is None:
+            break
 
-# Define agent
+if __name__ == '__main__':
+    # Parse arguments
 
-model_dir = utils.get_model_dir(args.model)
-agent = utils.Agent(args.env, env.observation_space, model_dir, args.argmax)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", default='MiniGrid-Empty-8x8-v0',
+                        help="name of the environment to be run (REQUIRED)")
+    parser.add_argument("--model", default='MiniGrid-Empty-8x8-v0_a2c_seed1_19-02-12-11-46-34',
+                        help="name of the trained model (REQUIRED)")
+    parser.add_argument("--seed", type=int, default=0,
+                        help="random seed (default: 0)")
+    parser.add_argument("--shift", type=int, default=0,
+                        help="number of times the environment is reset at the beginning (default: 0)")
+    parser.add_argument("--argmax", action="store_true", default=False,
+                        help="select the action with highest probability")
+    parser.add_argument("--pause", type=float, default=0.1,
+                        help="pause duration between two consequent actions of the agent")
+    args = parser.parse_args()
 
-# Run the agent
-
-done = True
-
-while True:
-    if done:
-        obs = env.reset()
-
-    time.sleep(args.pause)
-    renderer = env.render()
-
-    action = agent.get_action(obs)
-    obs, reward, done, _ = env.step(action)
-    agent.analyze_feedback(reward, done)
-
-    if renderer.window is None:
-        break
+    visualize_it(args.env,args.model,args.pause,args.seed,args.shift,args.argmax)
