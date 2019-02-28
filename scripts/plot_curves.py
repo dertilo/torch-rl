056b26193867865fb424e4404036b66808e214c5
Tilo Himmelsbach <tilo.himmelsbach@tu-berlin.de>
2019-02-28 11:45:42 +0100

    plot curves

--- scripts/plot_curves.py
+++ scripts/plot_curves.py
@@ -5,29 +5,30 @@ from numpy import genfromtxt
 from matplotlib import pyplot as plt
 
 
-def read_lines(file, mode ='b', encoding ='utf-8', limit=np.Inf):
-    counter = 0
-    with gzip.open(file, mode='r'+mode) if file.endswith('.gz') else open(file,mode='r'+mode) as f:
-        for line in f:
-            counter+=1
-            if counter>limit:
-                break
-            if mode == 'b':
-                yield line.decode(encoding).replace('\n','')
-            elif mode == 't':
-                yield line.replace('\n','')
-
 if __name__ == '__main__':
     path = '/home/tilo/code/RL/torch-rl/scripts/storage'
-    header = 'update,frames,FPS,duration,rreturn_mean,rreturn_std,rreturn_min,rreturn_max,num_frames_mean,num_frames_std,num_frames_min,num_frames_max,entropy,value,policy_loss,value_loss,grad_norm,return_mean,return_std,return_min,return_max'.split(',')
-
-    fig, axes = plt.subplots(1, 1)
+    # header = 'update,frames,FPS,duration,rreturn_mean,rreturn_std,rreturn_min,rreturn_max,num_frames_mean,num_frames_std,num_frames_min,num_frames_max,entropy,value,policy_loss,value_loss,grad_norm,return_mean,return_std,return_min,return_max'.split(',')
+    # header = 'update,duration,episodes,rewards,ep-len'.split(',')
+    with open(path+'/'+os.listdir(path)[0]+'/log.csv',mode='rt') as f:
+        cols = f.readline()[:-1].split(',')
+        header = [h for h in cols]
+        idx = cols.index('update')
+        header.pop(idx)
+        dur_idx = header.index('duration')
+        header.pop(dur_idx)
+    figs_axes = [plt.subplots(1, 1) for _ in header]
+    model_names = os.listdir(path)
 
     for dir_name in os.listdir(path):
-        # lines_g = read_lines(path+'/'+dir_name+'/log.csv')
-        # header = next(lines_g)
-        # data = [d.split(',') for d in lines_g]
-        my_data = genfromtxt(path+'/'+dir_name+'/log.csv', delimiter=',',skip_header=1)
-        axes.plot(my_data[:,header.index('update')],my_data[:,header.index('rreturn_mean')])
-    plt.legend([dir_name for dir_name in os.listdir(path)])
+        log_file = path + '/' + dir_name + '/log.csv'
+        my_data = genfromtxt(log_file, delimiter=',', skip_header=1)
+        for (fig,axes),param in zip(figs_axes,header):
+            axes.plot(my_data[:,dur_idx],my_data[:,cols.index(param)])
+            fig.suptitle(param)
+            axes.legend(model_names)
+
+    # for (fig,axes),param in zip(figs_axes,header):
+    #     handles, _ = axes.get_legend_handles_labels()
+        # axes.legend(handles, model_names)
+
     plt.show()
--- scripts/train_a2c.py
+++ scripts/train_a2c.py
@@ -18,37 +18,38 @@ except ImportError:
 
 env_name = 'MiniGrid-Snake-v0'
 # env_name = 'MiniGrid-Empty-8x8-v0'
-model_name = 'mlp-128-64'
-args = argparse.Namespace(**{
-    'model_name':model_name,
-    'env_name':env_name,
-    'seed':1,
-    'num_envs':16,
-    'num_batches':600,
-    'num_rollout_steps':5
+for i in [1]:
+    model_name = 'snake-a2c-%d'%i
+    args = argparse.Namespace(**{
+        'model_name':model_name,
+        'env_name':env_name,
+        'seed':i,
+        'num_envs':16,
+        'num_batches':1000,
+        'num_rollout_steps':5
+    })
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-})
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model_dir = 'storage/'+args.model_name
 
-model_dir = 'storage/'+args.model_name
+    logger = get_logger(model_dir)
+    csv_file, csv_writer = get_csv_writer(model_dir)
 
-logger = get_logger(model_dir)
-csv_file, csv_writer = get_csv_writer(model_dir)
+    set_seeds(args.seed)
 
-set_seeds(args.seed)
+    # envs = build_SnakeEnv(num_envs=1, use_multiprocessing=False)
+    envs = build_SnakeEnv(num_envs=16, use_multiprocessing=True)
+    agent = SnakeA2CAgent(envs.observation_space, envs.action_space)
+    logger.info("Model successfully created\n")
+    logger.info("{}\n".format(agent))
 
-envs = build_SnakeEnv(num_envs=16, use_multiprocessing=True)
-agent = SnakeA2CAgent(envs.observation_space, envs.action_space)
-logger.info("Model successfully created\n")
-logger.info("{}\n".format(agent))
+    if torch.cuda.is_available():
+        agent.cuda()
+    logger.info("CUDA available: {}\n".format(torch.cuda.is_available()))
 
-if torch.cuda.is_available():
-    agent.cuda()
-logger.info("CUDA available: {}\n".format(torch.cuda.is_available()))
+    algo = A2CAlgo(envs, agent, num_rollout_steps=args.num_rollout_steps, discount=0.99, lr=5e-4, gae_lambda=0.95,
+                   entropy_coef=0.01, value_loss_coef=0.5, max_grad_norm=0.5, num_recurr_steps=1)
 
-algo = A2CAlgo(envs, agent, num_rollout_steps=args.num_rollout_steps, discount=0.99, lr=7e-4, gae_lambda=0.95,
-               entropy_coef=0.01, value_loss_coef=0.5, max_grad_norm=0.5, num_recurr_steps=1)
+    algo.train_model(args.num_batches,CsvLogger(csv_file,csv_writer,logger))
 
-algo.train_model(args.num_batches,CsvLogger(csv_file,csv_writer,logger))
-
-visualize_it(build_SnakeEnv(num_envs=1, use_multiprocessing=False),agent)
+# visualize_it(build_SnakeEnv(num_envs=1, use_multiprocessing=False),agent)
